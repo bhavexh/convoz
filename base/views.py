cf2fa8492fdb5e5f83cc68edb6f4ad4d64bf7241
bhavesh <bhaveshvaishnav555@gmail.com>
2023-08-23 03:14:32 +0530

    user can now register into our app

--- base/views.py
+++ base/views.py
@@ -5,24 +5,21 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
 
-# rooms = [
-#     {'id': 1, 'name': 'lets learn python!'},
-#     {'id': 2, 'name': 'Design with me'},
-#     {'id': 3, 'name': 'Frontend developers'},
-# ]
 
 
 def loginPage(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try: 
@@ -38,15 +35,29 @@ def loginPage(request):
         else:
             messages.error(request, "Username OR Password does not exists.")
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
 
-def home(request):
+def registerPage(request):
+    form = UserCreationForm()
 
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occur during registration')
+    return render(request, 'base/login_register.html', {'form': form})
+
+def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
     rooms = Room.objects.filter(
@@ -65,6 +76,7 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}            
     return render(request, 'base/room.html', context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
